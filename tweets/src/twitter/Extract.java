a42971617daff407576d563e4e857c2b502166a8
Faizan <goshikhan176@gmail.com>
2019-03-16 20:45:34 -0700

    first commit

--- tweets/src/twitter/Extract.java
+++ tweets/src/twitter/Extract.java
@@ -1,4 +1,4 @@
-/* Copyright (c) 2007-2016 MIT 6.005 course staff, all rights reserved.
+ /* Copyright (c) 2007-2016 MIT 6.005 course staff, all rights reserved.
  * Redistribution of original or derived work requires permission of course staff.
  */
 package twitter;
